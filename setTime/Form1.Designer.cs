649ded6e010765e9397b5230d4e653bbcc72f5ef
0x5e <0x5e@sina.cn>
2015-03-04 13:48:36 +0800

    first commit

---
+++ setTime/Form1.Designer.cs
@@ -0,0 +1,174 @@
+﻿namespace setTime
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.path = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lag = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.CustomFormat = "yyyy/MM/dd HH:mm:ss";
+            this.dateTimePicker1.Font = new System.Drawing.Font("宋体", 9F);
+            this.dateTimePicker1.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
+            this.dateTimePicker1.Location = new System.Drawing.Point(91, 39);
+            this.dateTimePicker1.MaxDate = new System.DateTime(2114, 12, 31, 0, 0, 0, 0);
+            this.dateTimePicker1.MinDate = new System.DateTime(2014, 1, 1, 0, 0, 0, 0);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(205, 21);
+            this.dateTimePicker1.TabIndex = 0;
+            this.dateTimePicker1.Value = new System.DateTime(2014, 1, 1, 0, 0, 0, 0);
+            // 
+            // path
+            // 
+            this.path.Location = new System.Drawing.Point(91, 12);
+            this.path.Name = "path";
+            this.path.Size = new System.Drawing.Size(176, 21);
+            this.path.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(265, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(31, 21);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "...";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(41, 96);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(70, 25);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "运行";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(143, 96);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(127, 25);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "创建桌面快捷方式";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(15, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(59, 12);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "选择程序:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(15, 45);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(65, 12);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "日期/时间:";
+            // 
+            // lag
+            // 
+            this.lag.Location = new System.Drawing.Point(91, 67);
+            this.lag.Name = "lag";
+            this.lag.Size = new System.Drawing.Size(38, 21);
+            this.lag.TabIndex = 7;
+            this.lag.Text = "10";
+            this.lag.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(15, 70);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(59, 12);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "退出延迟:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(135, 70);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(17, 12);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "秒";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(310, 132);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.lag);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.path);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.TextBox path;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox lag;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ setTime/Form1.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace setTime
+{
+    public partial class Form1 : Form
+    {
+        string[] args = null;
+        public Form1(string[] args)
+        {
+            InitializeComponent();
+            this.args = args;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            UAC.ByPass();
+
+            Object val;
+            val = Application.UserAppDataRegistry.GetValue("time");
+            if (val != null)
+            {
+                try
+                {
+                    dateTimePicker1.Value = DateTime.ParseExact(val.ToString(), dateTimePicker1.Format.ToString(), null);
+                }
+                catch(Exception)
+                {
+                    dateTimePicker1.Value = System.DateTime.Now;
+                }
+            }
+            else
+            {
+                dateTimePicker1.Value = System.DateTime.Now;
+            }
+            
+            val = null;
+            val = Application.UserAppDataRegistry.GetValue("path");
+            if (val != null)
+            {
+                path.Text = val.ToString();
+            }
+
+            val = null;
+            val = Application.UserAppDataRegistry.GetValue("lag");
+            if (val != null)
+            {
+                lag.Text = val.ToString();
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();//新建打开文件对话框
+            ofd.InitialDirectory = Environment.CurrentDirectory;//设置初始文件目录
+            ofd.Filter = "试用程序(*.exe)|*.exe|所有文件(*.*)|*.*";//设置打开文件类型
+            if (ofd.ShowDialog(this) == DialogResult.OK)
+            {
+                path.Text = ofd.FileName;//FileName就是要打开的文件路径  
+            }
+        }
+
+        [System.Runtime.InteropServices.StructLayout(System.Runtime.InteropServices.LayoutKind.Sequential)]
+        public class SystemTime
+        {
+            public ushort wYear;
+            public ushort wMonth;
+            public ushort wDayOfWeek;
+            public ushort wDay;
+            public ushort Whour;
+            public ushort wMinute;
+            public ushort wSecond;
+            public ushort wMilliseconds;
+
+        }
+        //调用Kernel32.DLL
+        [System.Runtime.InteropServices.DllImport("Kernel32.dll")]
+        public static extern void GetLocalTime(SystemTime st);
+        [System.Runtime.InteropServices.DllImport("Kernel32.dll")]
+        public static extern void SetLocalTime(SystemTime st);
+
+        DateTime last_time;
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Application.UserAppDataRegistry.SetValue("time", dateTimePicker1.Format.ToString());
+            Application.UserAppDataRegistry.SetValue("path",path.Text);
+            Application.UserAppDataRegistry.SetValue("lag", lag.Text);
+
+            SystemTime last = new SystemTime();
+            GetLocalTime(last);
+            last_time = new DateTime(last.wYear, last.wMonth, last.wDay, last.Whour, last.wMinute, last.wSecond);
+            
+            SystemTime set_time = new SystemTime();
+            set_time.wYear = (ushort)this.dateTimePicker1.Value.Year;
+            set_time.wMonth = (ushort)this.dateTimePicker1.Value.Month;
+            set_time.wDay = (ushort)this.dateTimePicker1.Value.Day;
+            set_time.Whour = (ushort)this.dateTimePicker1.Value.Hour;
+            set_time.wMinute = (ushort)this.dateTimePicker1.Value.Minute;
+            set_time.wSecond = (ushort)this.dateTimePicker1.Value.Second;
+            SetLocalTime(set_time);
+
+            System.Diagnostics.Process.Start(path.Text, "");
+            new System.Threading.Thread(lag_exit).Start(); 
+        }
+
+        void lag_exit()
+        {
+            System.Threading.Thread.Sleep(Convert.ToInt32(lag.Text)*1000);
+
+            DateTime nowtime = last_time.AddMilliseconds(Convert.ToInt32(lag.Text) * 1000);
+            SystemTime set_time = new SystemTime();
+            set_time.wYear = (ushort)nowtime.Year;
+            set_time.wMonth = (ushort)nowtime.Month;
+            set_time.wDay = (ushort)nowtime.Day;
+            set_time.Whour = (ushort)nowtime.Hour;
+            set_time.wMinute = (ushort)nowtime.Minute;
+            set_time.wSecond = (ushort)nowtime.Second;
+            SetLocalTime(set_time);
+
+            Application.Exit();
+        }
+
+    }
+}
